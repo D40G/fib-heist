0dfa2ceff7b737799a428d3072a9033326997b65
Pujan Sapkota <59447828+Pujan77@users.noreply.github.com>
2021-11-02 23:55:42 +0545

    Updated fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'FIB heist by Pujan'
+version '1.0.0'
+
+shared_scripts { 
+	'@qb-core/import.lua'
+}
+
+server_script 'fib_s.lua'
+client_script 'fib_c.lua'
